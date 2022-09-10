4259e93e7b66738d8ca142a7e062f12de35bcfc9
Sieryuu <sieryuu@gmail.com>
2022-09-10 20:52:13 +0700

    fix PenyakitSerializer using wrong object

--- referensi/views.py
+++ referensi/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from referensi.models import Penyakit, Puskesmas
-from referensi.serializer import PuskesmasSerializer
+from referensi.serializer import PenyakitSerializer, PuskesmasSerializer
 
 
 # Create your views here.
 class PenyakitViewSet(viewsets.ModelViewSet):
     queryset = Penyakit.objects.all()
-    serializer_class = Penyakit
+    serializer_class = PenyakitSerializer
 
 class PuskesmasViewSet(viewsets.ModelViewSet):
     queryset = Puskesmas.objects.all()
