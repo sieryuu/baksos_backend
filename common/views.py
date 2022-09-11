7725fb6811820ed8f8bb62aa25fd06ae97cfa912
moranomicon <denissazhang@gmail.com>
2022-09-11 14:52:38 +0700

    Add django filter

--- common/views.py
+++ common/views.py
@@ -5,11 +5,14 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from common.serializer import UserSerializer
+from django_filters import rest_framework as filters
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = '__all__'
 
     @action(detail=True, methods=["get"])
     def get_user_permission(self, request, pk=None):
