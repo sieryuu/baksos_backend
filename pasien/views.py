3e795881f64673158f03739c06e27d5f6adc1628
Sieryuu <sieryuu@gmail.com>
2022-09-17 15:20:27 +0700

    change status to status_kk

--- pasien/views.py
+++ pasien/views.py
@@ -121,14 +121,14 @@ class PasienViewSet(viewsets.ModelViewSet):
 
             pasien = self.get_object()
 
-            status = serializer.validated_data["status"]
+            status_kk = serializer.validated_data["status"]
             tanggal = serializer.validated_data.get("tanggal")
             jam = serializer.validated_data.get("jam")
             perhatian = serializer.validated_data.get("perhatian")
 
             PasienService.serah_kartu_kuning(
                 pasien=pasien,
-                status=status,
+                status=status_kk,
                 tanggal=tanggal,
                 jam=jam,
                 perhatian=perhatian,
@@ -146,9 +146,7 @@ class PasienViewSet(viewsets.ModelViewSet):
         try:
             pasien = self.get_object()
 
-            PasienService.batal_serah_kartu_kuning(
-                pasien=pasien
-            )
+            PasienService.batal_serah_kartu_kuning(pasien=pasien)
 
             return Response(f"Kartu Kuning pasien {pasien.nama} sudah dibatalkan!")
         except ValidationError as ex:
